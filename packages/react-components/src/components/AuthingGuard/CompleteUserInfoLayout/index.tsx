89bcf81ab1574db18321ba66440b73325a3a20e8
lixpng <l1968108962@gmail.com>
2020-12-23 10:49:04 +0800

    fix: 文案

--- packages/react-components/src/components/AuthingGuard/CompleteUserInfoLayout/index.tsx
+++ packages/react-components/src/components/AuthingGuard/CompleteUserInfoLayout/index.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from 'react'
+import { useGuardContext } from '../../../context/global/context'
 import { CompleteUserInfoForm } from '../Forms/CompleteUserInfoForm'
 
 import './style.less'
 
 export const CompleteUserInfoLayout: FC = () => {
+  const {
+    state: { guardTitle },
+  } = useGuardContext()
+
   return (
     <div>
       <h2 className="authing-guard-complete-info-title">完善用户信息</h2>
 
       <p className="authing-guard-complete-info-msg">
-        欢迎加入 示例应用 ，为了更好的使用体验，请先完善您的资料信息
+        欢迎加入 {guardTitle} ，为了更好的使用体验，请先完善您的资料信息
       </p>
 
       <CompleteUserInfoForm />
