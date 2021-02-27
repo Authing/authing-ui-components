c4c8ac27bf8763bc7e543990bb0f9863670a08d3
lixpng <l1968108962@gmail.com>
2021-02-27 14:42:31 +0800

    fix: vue rollup config

--- packages/vue-components/configs/rollup.config.base.js
+++ packages/vue-components/configs/rollup.config.base.js
@@ -62,6 +62,8 @@ export default {
           'GuardScenes',
           'LoginMethods',
           'RegisterMethods',
+          'authClient',
+          'initAuthClient',
         ],
       },
     }),
