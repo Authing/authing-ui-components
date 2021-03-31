c896f03dad7b00a771925eaafc617a20bf5ff325
donglyc <silencesoon@vip.qq.com>
2021-03-31 15:00:47 +0800

    fix:styles

--- packages/react-components/src/components/AuthingGuard/ToggleLang/index.tsx
+++ packages/react-components/src/components/AuthingGuard/ToggleLang/index.tsx
@@ -3,6 +3,7 @@ import { Dropdown, Menu, Space } from 'antd'
 import { LANG_MAP } from '../locales'
 import { useTranslation } from 'react-i18next'
 import { Lang } from '../types/Locales'
+import { IconFont } from '../IconFont'
 import './style.less'
 
 export const ToggleLang: FC = () => {
@@ -14,31 +15,34 @@ export const ToggleLang: FC = () => {
   const { i18n } = useTranslation()
 
   return (
-    <Dropdown
-      className="authing-toggle-language"
-      trigger={['click']}
-      overlay={
-        <Menu onClick={onClick}>
-          {LANG_MAP.map((item) => (
-            <Menu.Item key={item.value}>{item.label}</Menu.Item>
-          ))}
-        </Menu>
-      }
-    >
-      <Space
-        size={6}
-        style={{
-          alignSelf: 'center',
-          color: '#999',
-          marginTop: 24,
-          cursor: 'pointer',
-        }}
-      >
-        {
-          LANG_MAP.find((item) => item.value === (i18n.language || Lang.zhCn))
-            ?.label
+    <div className="authing-toggle-language-container">
+      <IconFont type="authing-yuyan" />
+      <Dropdown
+        className="authing-toggle-language"
+        trigger={['click']}
+        overlay={
+          <Menu onClick={onClick}>
+            {LANG_MAP.map((item) => (
+              <Menu.Item key={item.value}>{item.label}</Menu.Item>
+            ))}
+          </Menu>
         }
-      </Space>
-    </Dropdown>
+      >
+        <Space
+          size={6}
+          style={{
+            alignSelf: 'center',
+            color: '#999',
+            marginTop: 24,
+            cursor: 'pointer',
+          }}
+        >
+          {
+            LANG_MAP.find((item) => item.value === (i18n.language || Lang.zhCn))
+              ?.label
+          }
+        </Space>
+      </Dropdown>
+    </div>
   )
 }
