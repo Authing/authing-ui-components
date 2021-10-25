62de82ed206f36812680d1c93189ecaeb92cd002
Lucsun <stx1811@163.com>
2021-10-25 21:01:31 +0800

    fix

--- packages/react-components/src/components/index.ts
+++ packages/react-components/src/components/index.ts
@@ -1,9 +1,7 @@
 import { AuthingGuard } from './AuthingGuard'
-import { Guard } from './Guard'
 
 export * from './AuthingGuard/types'
 export * from './AuthingGuard/hooks'
 export type { AuthenticationClientOptions } from 'authing-js-sdk'
 
 export { AuthingGuard }
-export { Guard }
