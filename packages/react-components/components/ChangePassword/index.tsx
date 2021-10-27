18d77ac4f922056eece76c60ad83c4f7b41e910d
Lucsun <stx1811@163.com>
2021-10-27 17:31:54 +0800

    fix ci

--- packages/react-components/components/ChangePassword/index.tsx
+++ packages/react-components/components/ChangePassword/index.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { message } from 'antd'
-
-import { GuardModuleType } from '../Guard/module'
-import { FirstLoginReset } from './core/firstLoginReset'
+import React from 'react'
 
 import { ImagePro } from '../ImagePro'
 
 // 手动修改密码，并非「忘记密码」
 // 进入的场景是读取配置：1开了首次登录修改密码 || 2开了密码轮换
 export const GuardChangePassword = (props: any) => {
-  let publicConfig = props.config.__publicConfig__
+  // let publicConfig = props.config.__publicConfig__
 
   // const onReset = (res: any) => {
   //   let code = res.code
