d4fb2878deb8db5cca2e7693377521881faff14d
Yuri <djw7569@gmail.com>
2021-10-27 18:04:24 +0800

    save

--- packages/react-components/components/ChangePassword/index.tsx
+++ packages/react-components/components/ChangePassword/index.tsx
@@ -9,7 +9,9 @@ import { ImagePro } from '../ImagePro'
 // 手动修改密码，并非「忘记密码」
 // 进入的场景是读取配置：1开了首次登录修改密码 || 2开了密码轮换
 export const GuardChangePassword = (props: any) => {
-  let publicConfig = props.config.__publicConfig__
+  let { initData, config } = props
+  // console.log('props', props)
+  let publicConfig = config.__publicConfig__
 
   // const onReset = (res: any) => {
   //   let code = res.code
@@ -25,19 +27,19 @@ export const GuardChangePassword = (props: any) => {
     <div className="g2-view-container">
       <div className="g2-view-header">
         <ImagePro
-          src={props.config?.logo}
+          src={config?.logo}
           size={48}
           borderRadius={4}
           alt=""
           className="icon"
         />
-        <div className="title">欢迎访问 xxxxxx</div>
+        <div className="title">欢迎访问 {config.title}</div>
         <div className="title-explain">
           为了保障账号安全，请设置你的初始密码。
         </div>
       </div>
       <div className="g2-view-tabs">
-        {/* <ResetPassword onReset={onReset} publicConfig={publicConfig} /> */}
+        <FirstLoginReset initData={initData} publicConfig={publicConfig} />
       </div>
     </div>
   )
