ecb404291512a204dba6c16024835f36505048df
Lucsun <stx1811@163.com>
2021-10-27 21:08:35 +0800

    fix ci

---
+++ scripts/buildDemo.js
@@ -0,0 +1,16 @@
+var shell = require("shelljs");
+
+shell.cd("./packages/react-components");
+var react = shell.exec("npm run build:lib");
+if (react.code !== 0) {
+  shell.echo("React Build 失败了 🐛");
+  shell.exit(1);
+}
+shell.cd("../native-js");
+var native = shell.exec("npm run build:lib");
+if (native.code !== 0) {
+  shell.echo("Native Build 失败了 🐛");
+  shell.exit(1);
+}
+
+shell.exit(0);
