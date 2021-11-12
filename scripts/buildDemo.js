a3f5da07902a18807ef1b8b69573a12f19233958
Lucsun <stx1811@163.com>
2021-11-12 13:45:38 +0800

    Fix build demo 脚本

--- scripts/buildDemo.js
+++ scripts/buildDemo.js
@@ -3,14 +3,18 @@ var shell = require("shelljs");
 // shell.exec("npm run build:lib");
 
 shell.cd("./packages/react-components");
-var react = shell.exec("npm run build:lib");
+var react = shell.exec("yarn build:lib");
+
 if (react.code !== 0) {
   shell.echo("React Build 失败了 🐛");
   shell.exit(1);
 }
 
-shell.cd("../native-js");
-var native = shell.exec("npm run build:lib");
+shell.cd("-");
+shell.cd("./packages/native-js");
+
+var native = shell.exec("yarn build:lib");
+
 if (native.code !== 0) {
   shell.echo("Native Build 失败了 🐛");
   shell.exit(1);
