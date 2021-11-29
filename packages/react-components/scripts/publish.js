7fe3145b57ef5318834cddb9fe5b226cbaa2277b
Lucsun <stx1811@163.com>
2021-11-29 15:06:25 +0800

    Fix

--- packages/react-components/scripts/publish.js
+++ packages/react-components/scripts/publish.js
@@ -1,57 +1,21 @@
-var $ = require('shelljs')
-var Package = require('../package.json')
-
-var version = Package.version
-var name = Package.name
+var { version } = require('../package.json')
+const shell = require('shelljs')
 
 const getVersionTag = (version) => {
-  const tagArr = ['alpha', 'beta', 'rc']
-
-  const tagIndexArr = tagArr
-    .map((type) => version.search(type))
-    .filter((index) => index !== -1)
-
-  if (tagIndexArr.length === 0) return ''
-
-  const tagName = tagIndexArr[0]
-
-  return tagName
+  if (version.includes('-beta')) {
+    return 'beta'
+  } else if (version.includes('-rc')) {
+    return 'rc'
+  } else if (version.includes('-alpha')) {
+    return 'alpha'
+  } else {
+    return ''
+  }
 }
-
-var build = $.exec('yarn build:lib')
-
-if (build.code !== 0) {
-  $.echo('build 失败了～')
-  $.exit(1)
-}
-
-// $.exec(`git tab ${version}`)
-
 var tag = getVersionTag(version)
 
-let publish
-
 if (tag !== '') {
-  publish = $.exec(`npm publish --tag ${tag}`)
+  shell.exec(`npm publish --tag ${tag}`)
 } else {
-  publish = $.exec('npm publish')
+  shell.exec('npm publish')
 }
-
-if (publish.code !== 0) {
-  $.echo(`publish 失败了～`)
-  $.exit(1)
-}
-
-if ($.which('cnpm')) {
-  $.echo(`正在同步到 淘宝源......`)
-
-  $.exec(`cnpm sync name`)
-} else if ($.which('open')) {
-  $.exec(`open https://npmmirror.com/sync/${name}`)
-} else {
-  $.echo(`记得手动同步到淘宝源～`)
-
-  $.echo(`https://npmmirror.com/sync/${name}`)
-}
-
-$.exit(0)
