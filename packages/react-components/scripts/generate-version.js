30560f0a031fec24499f74f9f5a64230b96e101a
Lucsun <stx1811@163.com>
2021-11-29 14:04:07 +0800

    Fix 新增脚本

--- packages/react-components/scripts/generate-version.js
+++ packages/react-components/scripts/generate-version.js
@@ -5,9 +5,6 @@ const { version } = require('../package.json')
 
 fs.writeFileSync(
   path.join(__dirname, '..', 'components', 'version', 'version.tsx'),
-  `
-  const version = '${version}'
-  export default version
-  `,
+  `export default '${version}'`,
   'utf8'
 )
