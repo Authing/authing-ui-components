809d510482098c15e453a5a51be24a2de9e5983a
wanglu <wanglu@merieth.com>
2021-12-07 15:27:03 +0800

    fix:去掉前缀图标

--- packages/react-components/components/CompleteInfo/core/completeInfo.tsx
+++ packages/react-components/components/CompleteInfo/core/completeInfo.tsx
@@ -14,7 +14,6 @@ import { completeFieldsFilter } from '../utils'
 import { User } from 'authing-js-sdk'
 import CustomFormItem from '../../ValidatorRules'
 import { SendCode } from '../../SendCode'
-import { UserOutlined, SafetyOutlined } from '@ant-design/icons'
 import { fieldRequiredRule } from '../../_utils'
 
 export interface CompleteInfoProps {
@@ -171,7 +170,7 @@ export const CompleteInfo: React.FC<CompleteInfoProps> = (props) => {
             size="large"
             maxLength={11}
             placeholder={t('login.inputPhone')}
-            prefix={<UserOutlined style={{ color: '#878A95' }} />}
+            // prefix={<UserOutlined style={{ color: '#878A95' }} />}
           />
         </CustomFormItem.Phone>
         <Form.Item
@@ -193,7 +192,7 @@ export const CompleteInfo: React.FC<CompleteInfoProps> = (props) => {
               length: verifyCodeLength,
             })}
             maxLength={verifyCodeLength}
-            prefix={<SafetyOutlined style={{ color: '#878A95' }} />}
+            // prefix={<SafetyOutlined style={{ color: '#878A95' }} />}
             method="phone"
             fieldName="internal phone:phone"
             data={''}
@@ -220,7 +219,7 @@ export const CompleteInfo: React.FC<CompleteInfoProps> = (props) => {
             autoComplete="email"
             size="large"
             placeholder={t('login.inputEmail')}
-            prefix={<UserOutlined style={{ color: '#878A95' }} />}
+            // prefix={<UserOutlined style={{ color: '#878A95' }} />}
           />
         </CustomFormItem.Email>
         <Form.Item
@@ -241,7 +240,7 @@ export const CompleteInfo: React.FC<CompleteInfoProps> = (props) => {
               length: verifyCodeLength,
             })}
             maxLength={verifyCodeLength}
-            prefix={<SafetyOutlined style={{ color: '#878A95' }} />}
+            // prefix={<SafetyOutlined style={{ color: '#878A95' }} />}
             method="email"
             data={''}
             fieldName="internal email:email"
@@ -339,87 +338,6 @@ export const CompleteInfo: React.FC<CompleteInfoProps> = (props) => {
       })
     // eslint-disable-next-line
   }, [extendsFields, user])
-
-  // const formFields = extendsFields
-  //   .filter((each) => completeFieldsFilter(user as User, each))
-  //   .map((def) => {
-  //     const key = `${def.type} ${def.name}`
-  //     const label = i18n.language === 'zh-CN' ? def.label || def.name : def.name
-  //     const required = def.required || false
-  //     const rules = def.validateRules
-  //     const generateRules = () => {
-  //       const l = []
-  //       if (required) {
-  //         l.push({
-  //           required: true,
-  //           message: `${label} ${t('login.noEmpty')}`,
-  //         })
-  //       }
-  //       rules.forEach((rule) => {
-  //         switch (rule.type) {
-  //           case 'isNumber':
-  //             l.push({
-  //               type: 'number',
-  //               required,
-  //               message: rule.error || t('login.mustBeNumber'),
-  //             })
-  //             break
-  //           case 'regExp':
-  //             l.push({
-  //               required,
-  //               pattern: new RegExp(rule.content.replaceAll('/', '')),
-  //               message: rule.error,
-  //             })
-  //             break
-  //           default:
-  //             break
-  //         }
-  //       })
-  //       return l
-  //     }
-
-  //     const inputElement = () => {
-  //       if (
-  //         def.type === 'internal' &&
-  //         Object.keys(INTERNAL_INPUT_MAP).includes(def.name)
-  //       ) {
-  //         if (def.name === 'country') {
-  //           return INTERNAL_INPUT_MAP[def.name](contryList)
-  //         } else {
-  //           return INTERNAL_INPUT_MAP[def.name]({
-  //             required,
-  //           })
-  //         }
-  //       } else {
-  //         if (Object.keys(INPUT_MAP).includes(def.inputType)) {
-  //           if (def.inputType === 'select') {
-  //             const options =
-  //               fieldMetadata.find((field) => field.key === def.name)
-  //                 ?.options || []
-  //             return INPUT_MAP[def.inputType](options)
-  //           }
-  //           return INPUT_MAP[def.inputType]()
-  //         }
-  //         return <Input type="text" className="authing-g2-input" />
-  //       }
-  //     }
-  //     if (def.type === 'internal' && ['phone', 'email'].includes(def.name)) {
-  //       return inputElement()
-  //     } else {
-  //       return (
-  //         <Form.Item
-  //           className="authing-g2-input-form"
-  //           rules={generateRules()}
-  //           key={key}
-  //           name={key}
-  //           label={label}
-  //           style={{ marginBottom: 8 }}
-  //         >
-  //           {inputElement()}
-  //         </Form.Item>
-  //       )
-  //     }
-  //   })
 
   const [, onFinish] = useAsyncFn(async (values: any) => {
     submitButtonRef.current?.onSpin(true)
