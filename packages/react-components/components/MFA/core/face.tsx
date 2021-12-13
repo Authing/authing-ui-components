5062ddf1e76209de43e7dafecf06a07d76d91cef
zhouyafeng <zhouyafeng@authing.cn>
2021-12-13 17:39:24 +0800

    feat:upload

--- packages/react-components/components/MFA/core/face.tsx
+++ packages/react-components/components/MFA/core/face.tsx
@@ -193,7 +193,6 @@ export const MFAFace = (props: any) => {
     if (videoDom?.paused || videoDom?.ended || !isFaceDetectionModelLoaded()) {
       return
     }
-    debugger
     const options = getFaceDetectorOptions()
     const result = await detectSingleFace(videoDom, options)
 
