d9172f1d66f8ddb25bd13fd466528cb3abf57cf7
Lucsun <stx1811@163.com>
2022-03-14 19:02:44 +0800

    修改 Vue Guard 工程化问题，重新编写打包流程

---
+++ packages/vue-components/components/index.js
@@ -0,0 +1,25 @@
+import AuthingGuard from './AuthingGuard'
+import Guard from './Guard'
+import '@authing/native-js-ui-components/lib/index.min.css'
+
+const components = [
+	AuthingGuard,
+	Guard
+]
+// 全局注册
+const install = (app) => {
+	components.forEach(component => {
+		app.component(component.name, component)
+	})
+}
+
+// 局部注册
+export {
+	install,
+	Guard,
+	AuthingGuard
+}
+
+export default {
+	install
+}
