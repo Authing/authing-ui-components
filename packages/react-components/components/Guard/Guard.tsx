35f0051f89f832d209316a2b997144ed37a1f7eb
Lucsun <stx1811@163.com>
2022-03-31 18:57:44 +0800

    Fix

--- packages/react-components/components/Guard/Guard.tsx
+++ packages/react-components/components/Guard/Guard.tsx
@@ -1,9 +1,12 @@
+import { memo } from 'react'
+
 import { GuardEvents } from './event'
 import { IG2FCProps } from '../Type'
 import { GuardLocalConfig } from './config'
 import { GuardModuleType } from './module'
 import 'moment/locale/zh-cn'
 import { useRenderGuardCore } from './core/index'
+import isEqual from 'lodash/isEqual'
 
 export interface GuardProps extends GuardEvents, IG2FCProps {
   config?: Partial<GuardLocalConfig>
@@ -14,9 +17,12 @@ interface ModuleState {
   initData: any
 }
 
-export const Guard = (props: GuardProps) => {
-  const { config } = props
+const propsAreEqual = (pre: GuardProps, current: GuardProps) => {
+  return !isEqual(pre, current)
+}
 
+export const Guard = memo((props: GuardProps) => {
+  const { config } = props
   // 首页 init 数据
   const initState: ModuleState = {
     moduleName: config?.defaultScenes ?? GuardModuleType.LOGIN,
@@ -24,6 +30,7 @@ export const Guard = (props: GuardProps) => {
   }
 
   const renderGuard = useRenderGuardCore(props, initState)
+  console.log('Guard')
 
   return renderGuard
-}
+}, propsAreEqual)
