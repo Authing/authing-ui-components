a32beebde2535c8a21fcf9b35f42f85221cea644
熊灵 <xiongling@authing.cn>
2022-10-27 00:10:23 +0800

    fix: 手机发送验证码逻辑调整

--- packages/react-components/components/SendCode/SendCodeByPhone.tsx
+++ packages/react-components/components/SendCode/SendCodeByPhone.tsx
@@ -41,21 +41,38 @@ export const SendCodeByPhone: FC<SendCodeByPhoneProps> = (props) => {
   const sendPhone = async (phone: string, countryCode?: string) => {
     try {
       // await authClient.sendSmsCode(phone, countryCode, scene)
-      const { code, statusCode, message: msg } = await post(
-        '/api/v2/sms/send',
-        {
-          phone,
-          phoneCountryCode: countryCode,
-          scene,
-        }
-      )
+      /**
+       * 这个 post 方法请求时，接口报错或者超时 code 会返回 -1、-2，这个时候全局会给出提示，并且不会抛错
+       * 会继续走下面逻辑，不走 catch
+       * post 方法：packages/react-components/components/_utils/http.ts
+       * 响应拦截：packages/react-components/components/_utils/responseManagement/index.ts
+       */
+      const data = await post('/api/v2/sms/send', {
+        phone,
+        phoneCountryCode: countryCode,
+        scene,
+      })
+      const { code, statusCode, message: msg } = data
+      // 200 表示请求成功，不报错
       if (statusCode === 200 || code === 200) {
         return { status: true }
       } else {
-        if (!(code === -1 || code === -2)) {
-          message.error(msg)
+        // 由于使用项目中 post 方法进行请求，外层进行了处理，不会抛错，catch 不会走，在这对 code 进行针对处理
+        // 'ECONNABORTED' 不知道是不是只有 sdk 报错，所以我在接口这也写了，防止接口也会1报这个错
+        if ((code as any) === 'ECONNABORTED') {
+          message.error(t('login.sendCodeTimeout'))
+          return {
+            status: false,
+            error: data,
+          }
         }
-        return { status: false, error: msg }
+        // code 不是 -1、-2 时，并且 msg 存在， 那么进行报错
+        if (!(code === -1 || code === -2)) {
+          msg && message.error(msg)
+        }
+        // 这里看之前 sdk catch 中把整个 error对象传递的，所以报错时我就把整个返回值当作 error 对象传递了，而不是单独传递 message
+        // 看的本文件 117行 onPhoneSendError 函数接受的 error 类型是一个对象「只包含 code、message」
+        return { status: false, error: data }
       }
       // return { status: true }
     } catch (error: any) {
